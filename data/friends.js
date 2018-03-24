fd6a22c4d9331ffbd7b73272a5b8e1efcf68f5e9
Erika Ruby Delgado <erikarubydelgado@Erikas-MacBook-Pro.local>
2018-03-23 22:32:51 -0500

    final changes(?)

---
+++ data/friends.js
@@ -0,0 +1,59 @@
+// list of friends being used
+
+// remember to do mopdule.export with your data made 
+
+// use array of objects
+var friends = [
+    {
+        "name": "John",
+        "photo": "insertlink",
+        "scores": [
+            3,
+            4,
+            2,
+            5,
+            5,
+            1,
+            4,
+            3,
+            3,
+            2
+        ]
+    },
+
+    {
+        "name": "Jillian",
+        "photo": "insertlink",
+        "scores": [
+            1,
+            1,
+            2,
+            2,
+            3,
+            5,
+            4,
+            5,
+            1,
+            1
+        ]
+    },
+
+    {
+        "name": "Henry",
+        "photo": "insertlink",
+        "scores": [
+            4,
+            1,
+            5,
+            2,
+            1,
+            4,
+            1,
+            2,
+            1,
+            5
+        ]
+    }
+]
+
+module.exports = friends;
